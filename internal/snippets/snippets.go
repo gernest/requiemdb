26e434ccdb5c484becaad20b2a764e37e424a1bb
gernest <geofreyernest@live.com>
2024-03-25 11:06:09 +0300

    add snippets

--- internal/snippets/snippets.go
+++ internal/snippets/snippets.go
@@ -1,7 +1,161 @@
 package snippets
 
-import "github.com/dgraph-io/badger/v4"
+import (
+	"errors"
+
+	"github.com/cespare/xxhash/v2"
+	"github.com/dgraph-io/badger/v4"
+	"github.com/dgraph-io/ristretto"
+	"github.com/dop251/goja"
+	v1 "github.com/requiemdb/requiemdb/gen/go/rq/v1"
+	"github.com/requiemdb/requiemdb/internal/compile"
+	"github.com/requiemdb/requiemdb/internal/compress"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+var (
+	ErrSnippetNotFound = errors.New("snippet not found")
+)
 
 type Snippets struct {
-	db *badger.DB
+	db    *badger.DB
+	cache *ristretto.Cache
+}
+
+func New(db *badger.DB, cacheBudget int64) (*Snippets, error) {
+	if cacheBudget == 0 {
+		cacheBudget = 26 << 20
+	}
+	cache, err := ristretto.NewCache(&ristretto.Config{
+		NumCounters: 1e7,
+		MaxCost:     cacheBudget,
+		BufferItems: 64,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &Snippets{db: db, cache: cache}, nil
+}
+
+func (s *Snippets) Close() error {
+	s.cache.Close()
+	return nil
+}
+
+func (s *Snippets) GetProgram(name string) (*goja.Program, error) {
+	hash := xxhash.Sum64String(name)
+	if o, ok := s.cache.Get(hash); ok {
+		return o.(*goja.Program), nil
+	}
+	key := buildKey(name)
+	var program *goja.Program
+	var cost int
+	err := s.db.View(func(txn *badger.Txn) error {
+		it, err := txn.Get(key)
+		if err != nil {
+			return err
+		}
+		var code v1.Snippet
+		err = it.Value(func(val []byte) error {
+			return proto.Unmarshal(val, &code)
+		})
+		if err != nil {
+			return err
+		}
+		data, err := compress.Decompress(code.Compiled)
+		if err != nil {
+			return err
+		}
+		program, err = goja.Compile(code.Name, string(data), true)
+		cost = len(data)
+		return err
+	})
+	if err != nil {
+		if errors.Is(err, badger.ErrKeyNotFound) {
+			return nil, ErrSnippetNotFound
+		}
+		return nil, err
+	}
+	s.cache.Set(hash, program, int64(cost))
+	return program, nil
+}
+
+func buildKey(name string) []byte {
+	key := make([]byte, 9)
+	key[len(key)-1] = byte(v1.SampleKind_SNIPPETS)
+	key = append(key, []byte(name)...)
+	return key
+}
+
+func (s *Snippets) Upsert(name string, data []byte) error {
+	raw, compiled, err := s.build(name, data)
+	if err != nil {
+		return err
+	}
+	now := timestamppb.Now()
+
+	key := buildKey(name)
+
+	err = s.db.Update(func(txn *badger.Txn) error {
+		var code *v1.Snippet
+		it, err := txn.Get(key)
+		if err != nil {
+			if !errors.Is(err, badger.ErrKeyNotFound) {
+				return err
+			}
+			code = &v1.Snippet{
+				Name:      name,
+				Raw:       raw,
+				Compiled:  compiled,
+				CreatedAt: now,
+				UpdatedAt: now,
+			}
+		} else {
+			code = &v1.Snippet{}
+			err = it.Value(func(val []byte) error {
+				return proto.Unmarshal(val, code)
+			})
+			if err != nil {
+				return err
+			}
+			code.Raw = raw
+			code.Compiled = compiled
+			code.UpdatedAt = now
+		}
+		o, err := proto.Marshal(code)
+		if err != nil {
+			return err
+		}
+		return txn.Set(key, o)
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *Snippets) build(name string, data []byte) (raw, compiled []byte, err error) {
+	compiled, err = compile.Compile(data)
+	if err != nil {
+		return
+	}
+	// make sure it is a valid goja program
+	progam, err := goja.Compile(name, string(compiled), true)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	raw, err = compress.Compress(data)
+	if err != nil {
+		return
+	}
+	compiled, err = compress.Compress(compiled)
+	if err != nil {
+		return
+	}
+	// We normally create/update then execute snippets. Put the compiled program in cache
+	// for faster loading
+	s.cache.Set(xxhash.Sum64String(name), progam, int64(len(compiled)))
+	return
 }
