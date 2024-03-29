c091f52926209712e444215728c42f06318bbb92
gernest <geofreyernest@live.com>
2024-03-29 21:58:44 +0300

    use exec as entry point

--- internal/compile/compile.go
+++ internal/compile/compile.go
@@ -25,6 +25,7 @@ func Compile(data []byte) ([]byte, error) {
 	result := api.Build(api.BuildOptions{
 		Bundle:        true,
 		Alias:         alias,
+		GlobalName:    "exec",
 		AbsWorkingDir: dir,
 		EntryPoints: []string{
 			"index.ts",
