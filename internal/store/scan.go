9e7b8562075e9410362109e5066d02ce4d0b41b4
gernest <geofreyernest@live.com>
2024-03-30 04:25:40 +0300

    new scanner

--- internal/store/scan.go
+++ internal/store/scan.go
@@ -3,7 +3,6 @@ package store
 import (
 	"encoding/binary"
 	"errors"
-	"io"
 	"math"
 
 	"github.com/RoaringBitmap/roaring/roaring64"
@@ -14,14 +13,75 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-type ScanFn func(sample *v1.Sample) error
+type Scanner struct {
+	it      *sampleIter
+	missing roaring64.Bitmap
+	config  *v1.Scan
+	key     [8 + 1 + 8]byte
+}
 
-func Scan(db *badger.DB, missing *roaring64.Bitmap, scan *v1.Scan, samples *lsm.Samples, f ScanFn) error {
-	txn := db.NewTransaction(false)
-	defer txn.Discard()
-	lbl := build(v1.RESOURCE(scan.Scope), scan.Filters)
-	defer lbl.Release()
+func NewScanner(db *badger.DB, scan *v1.Scan, samples *lsm.Samples) *Scanner {
+	return &Scanner{
+		it:     newIter(db.NewTransaction(false), scan, samples),
+		config: scan,
+	}
+}
 
+func (s *Scanner) Missing() *roaring64.Bitmap {
+	return &s.missing
+}
+
+func (s *Scanner) Close() {
+	s.it.Close()
+}
+
+func (s *Scanner) HasNext() bool {
+	return s.it.HasNext()
+}
+
+func (s *Scanner) Next() (*v1.Data, error) {
+	ns, id := s.it.Next()
+	copy(s.key[:], ns[:])
+	s.key[8] = byte(s.config.Scope)
+	copy(s.key[9:], id)
+
+	it, err := s.it.txn.Get(s.key[:])
+	if err != nil {
+		if errors.Is(err, badger.ErrKeyNotFound) {
+			s.missing.Add(s.it.sample)
+			return nil, nil
+		}
+		return nil, err
+	}
+	var sample v1.Sample
+	err = it.Value(func(val []byte) error {
+		return proto.Unmarshal(val, &sample)
+	})
+	if err != nil {
+		return nil, err
+	}
+	return sample.Data, nil
+}
+
+type sampleIter struct {
+	txn     *badger.Txn
+	b       *lsm.Iterator
+	date    uint64
+	r       *roaring64.Bitmap
+	reverse bool
+	it      roaring64.IntIterable64
+	d       [8]byte
+	sample  uint64
+	id      [8]byte
+	labels  *labels.Labels
+	err     error
+}
+
+func newIter(txn *badger.Txn, scan *v1.Scan, samples *lsm.Samples) *sampleIter {
+	i := &sampleIter{}
+	i.txn = txn
+	i.reverse = scan.Reverse
+	i.labels = build(v1.RESOURCE(scan.Scope), scan.Filters)
 	limit := scan.Limit
 	if limit == 0 {
 		limit = math.MaxUint64
@@ -30,91 +90,78 @@ func Scan(db *badger.DB, missing *roaring64.Bitmap, scan *v1.Scan, samples *lsm.
 	if scan.Reverse {
 		it = samples.ReverseIterator()
 	}
-	var b [8]byte
-	for it.HasNext() && limit > 0 {
-		date, r := it.Next()
-		binary.LittleEndian.PutUint64(b[:], date)
-		clone := r.Clone()
-		if len(scan.Filters) != 0 {
-			part, err := loadPartition(txn, &b, lbl)
-			if err != nil {
-				return err
-			}
-			clone.And(part)
-			if clone.IsEmpty() {
-				// Skip the whole partition. No sample satisfy our filter condition.
-				continue
-			}
-		}
-		nxit := clone.ReverseIterator()
-		if !scan.Reverse {
-			nxit = clone.Iterator()
-		}
-		var key [8 + 1 + 8]byte
-		copy(key[:], b[:])
-		key[8] = byte(scan.Scope)
-
-		for nxit.HasNext() && limit > 0 {
-			id := nxit.Next()
-			binary.LittleEndian.PutUint64(key[9:], id)
-			err := loadSample(txn, missing, id, key[:], f)
-			if err != nil {
-				if errors.Is(err, io.EOF) {
-					return nil
-				}
-				return err
-			}
-			limit--
-		}
-	}
-	return nil
+	i.b = it
+	return i
 }
 
-func loadSample(txn *badger.Txn, missing *roaring64.Bitmap, id uint64, key []byte, f ScanFn) error {
-	it, err := txn.Get(key)
-	if err != nil {
-		if errors.Is(err, badger.ErrKeyNotFound) {
-			missing.Add(id)
-			return nil
-		}
-		return err
-	}
-	return it.Value(func(val []byte) error {
-		var b v1.Sample
-		err := proto.Unmarshal(val, &b)
-		if err != nil {
-			return err
-		}
-		return f(&b)
-	})
+func (p *sampleIter) Close() {
+	p.txn.Discard()
+	p.labels.Release()
+	p.labels = nil
 }
 
-func loadPartition(txn *badger.Txn, part *[8]byte, lbl *labels.Labels) (b *roaring64.Bitmap, err error) {
-	for i := range lbl.Values {
-		if i == 0 {
-			b, err = loadLabel(txn, lbl.Values[i].Namespaced(part))
-			if err != nil {
-				return nil, err
-			}
-			continue
+func (p *sampleIter) Err() error { return p.err }
+
+func (p *sampleIter) HasNext() bool {
+	if p.err != nil {
+		return false
+	}
+	if p.r == nil {
+		p.date, p.r = p.b.Next()
+		p.it = p.r.ReverseIterator()
+		if !p.reverse {
+			p.it = p.r.Iterator()
 		}
-		o, err := loadLabel(txn, lbl.Values[i].Namespaced(part))
+		binary.LittleEndian.PutUint64(p.d[:], p.date)
+		p.loadPartition()
+	}
+	if !p.it.HasNext() {
+		if !p.b.HasNext() {
+			// we are done
+			return false
+		}
+		p.date, p.r = p.b.Next()
+		p.it = p.r.ReverseIterator()
+		if !p.reverse {
+			p.it = p.r.Iterator()
+		}
+		binary.LittleEndian.PutUint64(p.d[:], p.date)
+		p.loadPartition()
+	}
+	return p.it.HasNext()
+}
+
+func (p *sampleIter) Next() (date *[8]byte, id []byte) {
+	p.sample = p.it.Next()
+	binary.LittleEndian.PutUint64(p.id[:], p.sample)
+	return &p.d, p.id[:]
+}
+
+func (p *sampleIter) loadPartition() {
+	o := new(roaring64.Bitmap)
+	for _, v := range p.labels.Values {
+		err := loadLabel(p.txn, v.Namespaced(&p.d), o)
 		if err != nil {
-			return nil, err
+			p.r.Clear()
+			if errors.Is(err, badger.ErrKeyNotFound) {
+				// skip this partition
+				return
+			}
+			p.err = err
+			return
 		}
-		b.And(o)
+		p.r.And(o)
+		if p.r.IsEmpty() {
+			return
+		}
 	}
 	return
 }
 
-func loadLabel(txn *badger.Txn, key []byte) (o *roaring64.Bitmap, err error) {
-	o = new(roaring64.Bitmap)
+func loadLabel(txn *badger.Txn, key []byte, o *roaring64.Bitmap) (err error) {
 	it, err := txn.Get(key)
 	if err != nil {
-		if errors.Is(err, badger.ErrKeyNotFound) {
-			return o, nil
-		}
-		return nil, err
+		return err
 	}
 	err = it.Value(func(val []byte) error {
 		return o.UnmarshalBinary(val)
