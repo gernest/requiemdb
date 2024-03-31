99a7ea98f2c623b6dac314eb2bdfbada489501c8
gernest <geofreyernest@live.com>
2024-03-31 15:03:34 +0300

    collapse sidebar

--- ui/src/pages/console/index.tsx
+++ ui/src/pages/console/index.tsx
@@ -1,18 +1,36 @@
-import { Box, Button, PageLayout } from '@primer/react';
+import { Box, Button, Heading, IconButton, PageLayout, Tooltip } from '@primer/react';
 import { Editor } from '../../components';
-import { FileIcon } from '@primer/octicons-react';
+import { FileIcon, SidebarCollapseIcon, SidebarExpandIcon } from '@primer/octicons-react';
+import { useState } from 'react';
 
 
 export const Console = () => {
+    const [expand, setExpand] = useState<boolean>(false)
     return (
         <PageLayout containerWidth='full'>
-            <PageLayout.Header>
+            <PageLayout.Content>
                 <Box display={"flex"}
                     justifyContent={"space-between"}
                     maxWidth={"100%"}
+                    mb={3}
                 >
+
                     <Box display={"flex"} alignItems={"center"}
                     >
+                        <Box mr={1}>
+                            {!expand && (
+                                <Tooltip text="Expand file tree">
+                                    <IconButton aria-label='Expand file tree' icon={SidebarExpandIcon}
+                                        onClick={() => setExpand(true)} />
+                                </Tooltip>
+                            )}
+                            {expand && (
+                                <Tooltip text="Collapse file tree">
+                                    <IconButton aria-label='Collapse file tree' icon={SidebarCollapseIcon}
+                                        onClick={() => setExpand(false)} />
+                                </Tooltip>
+                            )}
+                        </Box>
                         <Button
                             leadingVisual={FileIcon}
                         >http_request_total.ts</Button>
@@ -28,13 +46,11 @@ export const Console = () => {
                         <Button sx={{ marginLeft: "12px" }} variant='primary'>save changes</Button>
                     </Box>
                 </Box>
-            </PageLayout.Header>
-            <PageLayout.Content>
                 <Editor width={"100%"} minHeight={400} />
             </PageLayout.Content>
-            <PageLayout.Pane position={"start"}>
+            {expand && (<PageLayout.Pane position={"start"} width={"small"}>
                 Tree
-            </PageLayout.Pane>
+            </PageLayout.Pane>)}
         </PageLayout>
 
     )
