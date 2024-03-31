79a2b7a00537dfe8e1072e10597077732edd3aee
gernest <geofreyernest@live.com>
2024-03-31 04:48:16 +0300

    initial commit

--- ui/src/pages/console/index.tsx
+++ ui/src/pages/console/index.tsx
@@ -1,10 +1,34 @@
-import { Box } from '@primer/react';
+import { Box, Button } from '@primer/react';
 import { Editor } from '../../components';
+import { FileIcon } from '@primer/octicons-react';
 
 
 export const Console = () => {
     return (
         <Box>
+            <Box display={"flex"}
+                justifyContent={"space-between"}
+                mb={3}
+                pb={2}
+                maxWidth={"100%"}
+            >
+                <Box display={"flex"} alignItems={"center"}
+                >
+                    <Button
+                        leadingVisual={FileIcon}
+                    >http_request_total.ts</Button>
+
+                </Box>
+                <Box
+                    display={"flex"}
+                    alignItems={"center"}
+                    flexDirection={"row"}
+                    px={1}
+                >
+                    <Button>cancel changes</Button>
+                    <Button sx={{ marginLeft: "12px" }} variant='primary'>save changes</Button>
+                </Box>
+            </Box>
             <Editor width={"100%"} minHeight={400} />
         </Box>
     )
