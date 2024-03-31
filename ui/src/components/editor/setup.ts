048688a0d231512b7c13e4793eaa81e7e3f374bd
gernest <geofreyernest@live.com>
2024-03-31 18:51:42 +0300

    generate rq definition files

--- ui/src/components/editor/setup.ts
+++ ui/src/components/editor/setup.ts
@@ -2,6 +2,7 @@
 import * as monaco from 'monaco-editor';
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+import { requiem } from "./defs.json";
 /** define MonacoEnvironment.getWorker  */
 (self as any).MonacoEnvironment = {
     getWorker(_: any, label: string) {
@@ -12,3 +13,6 @@ import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
     }
 };
 monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
+console.log(requiem);
+monaco.languages.typescript.typescriptDefaults.addExtraLib(requiem)
+
--- ui/ui.go
+++ ui/ui.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 )
 
+//go:generate go run ../internal/generate/def/main.go ../packages/rq/dist/rq.d.ts src/components/editor/defs.ts
 //go:generate npm run build
 //go:embed dist
 var FS embed.FS
