38d3759eabae780c1000e74d5ab5fb8650eb2ffe
gernest <geofreyernest@live.com>
2024-03-31 14:21:59 +0300

    update console layout

--- ui/src/pages/console/index.tsx
+++ ui/src/pages/console/index.tsx
@@ -1,35 +1,41 @@
-import { Box, Button } from '@primer/react';
+import { Box, Button, PageLayout } from '@primer/react';
 import { Editor } from '../../components';
 import { FileIcon } from '@primer/octicons-react';
 
 
 export const Console = () => {
     return (
-        <Box>
-            <Box display={"flex"}
-                justifyContent={"space-between"}
-                mb={3}
-                pb={2}
-                maxWidth={"100%"}
-            >
-                <Box display={"flex"} alignItems={"center"}
+        <PageLayout containerWidth='full'>
+            <PageLayout.Header>
+                <Box display={"flex"}
+                    justifyContent={"space-between"}
+                    maxWidth={"100%"}
                 >
-                    <Button
-                        leadingVisual={FileIcon}
-                    >http_request_total.ts</Button>
+                    <Box display={"flex"} alignItems={"center"}
+                    >
+                        <Button
+                            leadingVisual={FileIcon}
+                        >http_request_total.ts</Button>
 
+                    </Box>
+                    <Box
+                        display={"flex"}
+                        alignItems={"center"}
+                        flexDirection={"row"}
+                        px={1}
+                    >
+                        <Button>cancel changes</Button>
+                        <Button sx={{ marginLeft: "12px" }} variant='primary'>save changes</Button>
+                    </Box>
                 </Box>
-                <Box
-                    display={"flex"}
-                    alignItems={"center"}
-                    flexDirection={"row"}
-                    px={1}
-                >
-                    <Button>cancel changes</Button>
-                    <Button sx={{ marginLeft: "12px" }} variant='primary'>save changes</Button>
-                </Box>
-            </Box>
-            <Editor width={"100%"} minHeight={400} />
-        </Box>
+            </PageLayout.Header>
+            <PageLayout.Content>
+                <Editor width={"100%"} minHeight={400} />
+            </PageLayout.Content>
+            <PageLayout.Pane position={"start"}>
+                Tree
+            </PageLayout.Pane>
+        </PageLayout>
+
     )
 }
