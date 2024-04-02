8f1a4d50c944a8f8c57af2b91a60f4a94c7e984e
gernest <geofreyernest@live.com>
2024-04-02 23:50:06 +0300

    update example

--- packages/examples/src/metrics/index.ts
+++ packages/examples/src/metrics/index.ts
@@ -1,16 +1,18 @@
-import { Metrics, render } from "@requiemdb/rq";
+import { metrics, render } from "@requiemdb/rq";
 
 /**
  *  Instant Vectors
  */
 render(
-    (new Metrics("http_requests_total"))
+    metrics()
+        .name("http_requests_total")
         .query(),
 )
 
 // With attributes filter
 render(
-    (new Metrics("http_requests_total"))
+    metrics()
+        .name("http_requests_total")
         .attr("job", "rq")
         .attr("group", "canary")
         .query(),
@@ -20,7 +22,8 @@ render(
  * Range Vectors
  */
 render(
-    (new Metrics("http_requests_total"))
+    metrics()
+        .name("http_requests_total")
         .attr("job", "rq")
         .ago("5m")
         .query(),
