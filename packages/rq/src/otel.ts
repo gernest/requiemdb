589e0c9840d0e2f8ebc3040ee0947cd8ea3b946e
gernest <geofreyernest@live.com>
2024-04-13 00:56:29 +0300

    update package

--- packages/rq/src/otel.ts
+++ packages/rq/src/otel.ts
@@ -31,30 +31,11 @@ export interface Span {
 }
 
 export enum SpanKind {
-    // Unspecified. Do NOT use as default.
-    // Implementations MAY assume SpanKind to be INTERNAL when receiving UNSPECIFIED.
     SPAN_KIND_UNSPECIFIED = 0,
-
-    // Indicates that the span represents an internal operation within an application,
-    // as opposed to an operation happening at the boundaries. Default value.
     SPAN_KIND_INTERNAL = 1,
-
-    // Indicates that the span covers server-side handling of an RPC or other
-    // remote network request.
     SPAN_KIND_SERVER = 2,
-
-    // Indicates that the span describes a request to some remote service.
     SPAN_KIND_CLIENT = 3,
-
-    // Indicates that the span describes a producer sending a message to a broker.
-    // Unlike CLIENT and SERVER, there is often no direct critical path latency relationship
-    // between producer and consumer spans. A PRODUCER span ends when the message was accepted
-    // by the broker while the logical processing of the message might span a much longer time.
     SPAN_KIND_PRODUCER = 4,
-
-    // Indicates that the span describes consumer receiving a message from a broker.
-    // Like the PRODUCER kind, there is often no direct critical path latency relationship
-    // between producer and consumer spans.
     SPAN_KIND_CONSUMER = 5,
 }
 
@@ -78,12 +59,8 @@ export interface Status {
 
 
 export enum StatusCode {
-    // The default status.
     STATUS_CODE_UNSET = 0,
-    // The Span has been validated by an Application developer or Operator to 
-    // have completed successfully.
     STATUS_CODE_OK = 1,
-    // The Span contains an error.
     STATUS_CODE_ERROR = 2,
 };
 
