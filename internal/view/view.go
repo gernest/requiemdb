5c34786b406e57c555a5d3c1feb4e454217a3897
gernest <geofreyernest@live.com>
2024-04-21 00:34:31 +0300

    use quantum to generate views

--- internal/view/view.go
+++ internal/view/view.go
@@ -1,40 +1,24 @@
 package view
 
 import (
-	"fmt"
 	"time"
 
+	"github.com/gernest/rbf/quantum"
 	"github.com/gernest/requiemdb/internal/x"
 	"github.com/gernest/roaring/shardwidth"
 )
 
-type Quantum byte
-
 const (
-	Y Quantum = iota
-	M
-	D
+	stdView = "std"
+
+	// keep views for year, month, day and hour.
+	stdQuantum = quantum.TimeQuantum("YMDH")
 )
 
-func (q Quantum) Format(t time.Time) string {
-	switch q {
-	case Y:
-		return t.Format("2006")
-	case M:
-		return t.Format("200601")
-	case D:
-		return t.Format("20060102")
-	default:
-		return t.Format("20060102")
-	}
-}
-
 func Std(t time.Time) []string {
-	return []string{
-		fmt.Sprintf("std_%s", Y.Format(t)),
-		fmt.Sprintf("std_%s", M.Format(t)),
-		fmt.Sprintf("std_%s", D.Format(t)),
-	}
+	return quantum.ViewsByTime(
+		stdView, t, stdQuantum,
+	)
 }
 
 // Pos generates position in a bitmap
