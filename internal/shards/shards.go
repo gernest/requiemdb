592621c9719c337ac51ec1b131513b39e2bc0db6
gernest <geofreyernest@live.com>
2024-04-24 05:57:40 +0300

    add simpler shards package
    Opens database per shard

---
+++ internal/shards/shards.go
@@ -0,0 +1,95 @@
+package shards
+
+import (
+	"errors"
+	"log/slog"
+	"path/filepath"
+	"strconv"
+	"sync"
+
+	"github.com/gernest/rbf"
+	"github.com/gernest/rbf/cfg"
+)
+
+type Shards struct {
+	config cfg.Config
+	mu     sync.RWMutex
+	db     map[uint64]*rbf.DB
+	dir    string
+}
+
+func New(path string, config *cfg.Config) *Shards {
+	if config == nil {
+		config = cfg.NewDefaultConfig()
+	}
+	return &Shards{
+		db:     make(map[uint64]*rbf.DB),
+		dir:    path,
+		config: *config,
+	}
+}
+
+func (s *Shards) View(shard uint64, f func(tx *rbf.Tx) error) error {
+	db, err := s.get(shard)
+	if err != nil {
+		return err
+	}
+	tx, err := db.Begin(false)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	return f(tx)
+}
+
+func (s *Shards) Update(shard uint64, f func(tx *rbf.Tx) error) error {
+	db, err := s.get(shard)
+	if err != nil {
+		return err
+	}
+	tx, err := db.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	err = f(tx)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit()
+}
+
+func (s *Shards) get(shard uint64) (*rbf.DB, error) {
+	s.mu.RLock()
+	r, ok := s.db[shard]
+	if ok {
+		s.mu.RUnlock()
+		return r, nil
+	}
+	s.mu.RUnlock()
+	path := filepath.Join(s.dir, strconv.FormatUint(shard, 10))
+	config := s.config
+	config.Logger = slog.Default().With(
+		slog.Uint64("shard", shard),
+	)
+	r = rbf.NewDB(path, &config)
+	err := r.Open()
+	if err != nil {
+		return nil, err
+	}
+	s.mu.Lock()
+	s.db[shard] = r
+	s.mu.Unlock()
+	return r, nil
+}
+
+func (s *Shards) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	errs := make([]error, 0, len(s.db))
+	for _, r := range s.db {
+		errs = append(errs, r.Close())
+	}
+	return errors.Join(errs...)
+}
