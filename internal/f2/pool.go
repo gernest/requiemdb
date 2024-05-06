bc8027d1ca9546284f7119209092b7e77d246ccc
gernest <geofreyernest@live.com>
2024-05-06 17:05:40 +0300

    pool resources

--- internal/f2/pool.go
+++ internal/f2/pool.go
@@ -3,7 +3,17 @@ package f2
 import (
 	"sync"
 
+	storev1 "github.com/gernest/requiemdb/gen/go/store/v1"
 	"google.golang.org/protobuf/proto"
+)
+
+var (
+	metricPool = NewPool(func() *storev1.Metric { return new(storev1.Metric) })
+	histPool   = NewPool(func() *storev1.HistogramDataPoint { return new(storev1.HistogramDataPoint) })
+	exHistPool = NewPool(func() *storev1.ExponentialHistogramDataPoint { return new(storev1.ExponentialHistogramDataPoint) })
+	sumtPool   = NewPool(func() *storev1.SummaryDataPoint { return new(storev1.SummaryDataPoint) })
+	logPool    = NewPool(func() *storev1.LogRecord { return new(storev1.LogRecord) })
+	spanPool   = NewPool(func() *storev1.Span { return new(storev1.Span) })
 )
 
 type Pool[T proto.Message] struct {
