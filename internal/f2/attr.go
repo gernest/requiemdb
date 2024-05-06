aa5c4cce373288189f6e3bcfb3bf917905d78536
gernest <geofreyernest@live.com>
2024-05-06 21:43:31 +0300

    build sum and gauge

--- internal/f2/attr.go
+++ internal/f2/attr.go
@@ -1,14 +1,21 @@
 package f2
 
 import (
+	"bytes"
 	"sync"
 
+	"github.com/RoaringBitmap/roaring/roaring64"
+	"github.com/cespare/xxhash/v2"
 	v1 "go.opentelemetry.io/proto/otlp/common/v1"
 	resouncev1 "go.opentelemetry.io/proto/otlp/resource/v1"
 )
 
 type Attr struct {
 	resource, scope, attr []*v1.KeyValue
+	rb, sb, ab            roaring64.Bitmap
+	tr                    Translate
+	b                     bytes.Buffer
+	hash                  xxhash.Digest
 }
 
 func newAttr() *Attr {
@@ -18,37 +25,76 @@ func newAttr() *Attr {
 func (a *Attr) Resource(r *resouncev1.Resource) {
 	a.resource = a.resource[:0]
 	a.resource = append(a.resource, r.GetAttributes()...)
+	a.do(&a.rb, a.resource)
 }
 
 func (a *Attr) Scope(scope *v1.InstrumentationScope) {
 	a.scope = a.scope[:0]
-	if a.scope != nil {
-		a.scope = append(a.scope, scope.GetAttributes()...)
-	}
+	a.scope = append(a.scope, scope.GetAttributes()...)
+	a.do(&a.sb, a.scope)
 }
 
-func (a *Attr) scoped() int {
-	return len(a.resource) + len(a.scope)
+func (a *Attr) Attr(name []byte, kv []*v1.KeyValue) ([]*v1.KeyValue, *roaring64.Bitmap, uint64) {
+	a.ab.Clear()
+	a.ab.Or(&a.rb)
+	a.ab.Or(&a.sb)
+	a.do(&a.ab, kv)
+	a.name(name)
+	a.attr = append(a.attr[:0], a.resource...)
+	a.attr = append(a.attr, a.scope...)
+	a.attr = append(a.attr, kv...)
+	a.hash.Reset()
+	a.ab.WriteTo(&a.hash)
+	return a.attr, &a.ab, a.hash.Sum64()
 }
 
-func (a *Attr) Attr(kv []*v1.KeyValue) []*v1.KeyValue {
-	if a.scoped() > 0 && len(a.attr) < a.scoped() {
-		a.attr = append(a.attr, a.resource...)
-		a.attr = append(a.attr, a.scope...)
-	}
-	a.attr = append(a.attr[:a.scoped()], kv...)
-	return a.attr
-}
-
-func (a *Attr) Reset() {
+func (a *Attr) Reset(tr Translate) *Attr {
 	a.resource = a.resource[:0]
 	a.scope = a.scope[:0]
 	a.attr = a.attr[:0]
+	a.rb.Clear()
+	a.sb.Clear()
+	a.ab.Clear()
+	a.b.Reset()
+	a.hash.Reset()
+	a.tr = tr
+	return a
 }
 
 func (a *Attr) Release() {
-	a.Reset()
+	a.Reset(nil)
 	attrPool.Put(a)
+}
+
+func (a *Attr) name(name []byte) {
+	a.b.Reset()
+	a.b.Write(nameKey)
+	a.b.Write(equal)
+	a.b.Write(name)
+	a.ab.Add(a.tr.Tr(a.b.Bytes()))
+}
+
+func (a *Attr) do(b *roaring64.Bitmap, kv []*v1.KeyValue) {
+	b.Clear()
+	for _, v := range kv {
+		a.b.Reset()
+		a.b.WriteString(v.Key)
+		a.b.Write(equal)
+		if toString(&a.b, v.Value) {
+			b.Add(a.tr.Tr(a.b.Bytes()))
+		}
+	}
+}
+
+func toString(b *bytes.Buffer, a *v1.AnyValue) bool {
+	switch e := a.Value.(type) {
+	case *v1.AnyValue_StringValue:
+
+		b.WriteString(e.StringValue)
+		return true
+	default:
+		return false
+	}
 }
 
 var attrPool = &sync.Pool{New: func() any { return new(Attr) }}
--- internal/f2/metrics.go
+++ internal/f2/metrics.go
@@ -1,8 +1,10 @@
 package f2
 
 import (
+	"github.com/RoaringBitmap/roaring/roaring64"
 	"github.com/gernest/arrow3"
 	v1 "github.com/gernest/requiemdb/gen/go/store/v1"
+	"github.com/gernest/roaring/shardwidth"
 	metricsv1 "go.opentelemetry.io/proto/otlp/metrics/v1"
 )
 
@@ -10,9 +12,21 @@ type ID interface {
 	Next() uint64
 }
 
+type Translate interface {
+	Tr([]byte) uint64
+}
+
+var (
+	attrPrefix = []byte("0:")
+	equal      = []byte("=")
+	nameKey    = []byte("__name__")
+)
+
 type Metrics struct {
-	build *arrow3.Schema[*v1.Metric]
-	id    ID
+	build     *arrow3.Schema[*v1.Metric]
+	id        ID
+	tr        Translate
+	positions roaring64.Bitmap
 }
 
 func (m *Metrics) Append(data *metricsv1.MetricsData) {
@@ -33,9 +47,11 @@ func (m *Metrics) Append(data *metricsv1.MetricsData) {
 
 				if v := ms.GetGauge(); v != nil {
 					mx.Kind = v1.Metric_GAUGE
+					m.gauge(mx, a, v)
 				}
 				if v := ms.GetSum(); v != nil {
 					mx.Kind = v1.Metric_SUM
+					m.sum(mx, a, v)
 				}
 				if v := ms.GetHistogram(); v != nil {
 					mx.Kind = v1.Metric_HISTOGRAM
@@ -51,4 +67,60 @@ func (m *Metrics) Append(data *metricsv1.MetricsData) {
 
 		}
 	}
+}
+
+func (m *Metrics) gauge(mx *v1.Metric, a *Attr, g *metricsv1.Gauge) {
+	name := []byte(mx.Name)
+	var columns *roaring64.Bitmap
+	for _, dp := range g.DataPoints {
+		mx.StartTimeUnixNano = dp.StartTimeUnixNano
+		mx.TimeUnixNano = dp.TimeUnixNano
+		mx.Exemplars = dp.Exemplars
+		switch e := dp.Value.(type) {
+		case *metricsv1.NumberDataPoint_AsDouble:
+			mx.Gauge = &e.AsDouble
+		case *metricsv1.NumberDataPoint_AsInt:
+			f := float64(e.AsInt)
+			mx.Gauge = &f
+		}
+		mx.Attributes, columns, mx.Hash = a.Attr(name, dp.Attributes)
+		id := m.id.Next()
+		mx.Id = id
+		it := columns.Iterator()
+		for it.HasNext() {
+			m.positions.Add(pos(id, it.Next()))
+		}
+		m.build.Append(mx)
+	}
+}
+
+func (m *Metrics) sum(mx *v1.Metric, a *Attr, g *metricsv1.Sum) {
+	name := []byte(mx.Name)
+	mx.AggregationTemporality = g.AggregationTemporality
+	mx.IsMonotonic = g.IsMonotonic
+	var columns *roaring64.Bitmap
+	for _, dp := range g.DataPoints {
+		mx.StartTimeUnixNano = dp.StartTimeUnixNano
+		mx.TimeUnixNano = dp.TimeUnixNano
+		mx.Exemplars = dp.Exemplars
+		switch e := dp.Value.(type) {
+		case *metricsv1.NumberDataPoint_AsDouble:
+			mx.Gauge = &e.AsDouble
+		case *metricsv1.NumberDataPoint_AsInt:
+			f := float64(e.AsInt)
+			mx.Gauge = &f
+		}
+		mx.Attributes, columns, mx.Hash = a.Attr(name, dp.Attributes)
+		id := m.id.Next()
+		mx.Id = id
+		it := columns.Iterator()
+		for it.HasNext() {
+			m.positions.Add(pos(id, it.Next()))
+		}
+		m.build.Append(mx)
+	}
+}
+
+func pos(row, col uint64) uint64 {
+	return row*shardwidth.ShardWidth + (col % shardwidth.ShardWidth)
 }
